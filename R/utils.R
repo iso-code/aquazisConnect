35fc0fb3700df97f525853888138be5d831528e5
Marcel <water-traits@github.com>
2025-08-28 15:36:36 +0200

    corrected function

--- R/utils.R
+++ R/utils.R
@@ -32,7 +32,7 @@ get_aquazis_meta <- function(hub){
 #' Optionally, a parameter can be provided to filter the request.
 #'
 #' @param hub Character. Base URL of the AQUAZIS hub.
-#' @param parameter List or NULL. Request parameters (e.g., \code{list(f_parameter="Abflusskurve")}).
+#' @param parameter character. Request parameters (e.g., \code{c("Wasserstand)}).
 #'
 #' @return A tibble with results or, for certain parameters, the raw JSON list.
 #' @export
@@ -42,10 +42,21 @@ get_aquazis_meta <- function(hub){
 #' get_aquazis_zrlist("https://aquazis.example.com")
 #' get_aquazis_zrlist("https://aquazis.example.com", list(f_parameter="Abflusskurve"))
 #' }
-get_aquazis_zrlist<-function(hub, parameter=NULL){
-  
+get_aquazis_zrlist<-function(hub, st_id="", parameter="Wasserstand"){
+
+
+  parameter <- list(
+    f_ort = st_id,
+      f_parameter = parameter,
+    f_defart = "K",
+    f_herkunft = "F",
+    f_reihenart = "Z",
+    f_quelle = "P"
+  )
+
+
   zr_list_url<- paste0(hub,"/zrlist_from_db")
-  
+
   if(!is.null(parameter)){
     ts_list<-do_aquazis_query(zr_list_url,parameter)
     ts_list<-fromJSON(readLines(ts_list,warn=FALSE))
@@ -54,14 +65,14 @@ get_aquazis_zrlist<-function(hub, parameter=NULL){
     else
       {return(ts_list$results)}
   }
-  
+
   else{
-   
+
     ts_list<-curl(zr_list_url)
     ts_list<-fromJSON(readLines(ts_list, warn=FALSE))
     return(tibble(ts_list$results))
   }
-  
+
 
 }
 
@@ -80,13 +91,31 @@ get_aquazis_zrlist<-function(hub, parameter=NULL){
 #' \dontrun{
 #' get_aquazis_zr("https://aquazis.example.com", list(f_parameter="Abfluss"))
 #' }
-get_aquazis_zr<-function(hub, parameter){
+get_aquazis_zr<-function(hub=NULL, zrid=NULL, begin="", end=""){
+
+  if (is.null(zrid) || is.na(zrid)) {
+    stop("Error: zrid NULL or NA")
+  }
+
+  if (is.null(hub) || is.na(hub)) {
+    stop("Error: Zrid NULL or NA")
+  }
+
+
+  parameter = list(zrid = zrid,
+                    von = begin,
+                    bis = end,
+                    zpform = "#Y#m#d#H#M#S",
+                    yform = "7.3f",
+                    compact = "true")
+
   zr_list_url<- paste0(hub,"/get_zr")
-  ts_data<-do_aquazis_query(zr_list_url,parameter)
+
+  ts_data<-create_aquazis_query(zr_list_url,parameter)
   ts_data<-fromJSON(readLines(ts_data,warn=FALSE))
   return(ts_data)
 }
-  
+
 #' Execute an AQUAZIS query
 #'
 #' Helper function to perform a request against an AQUAZIS hub and return the connection.
@@ -101,12 +130,12 @@ get_aquazis_zr<-function(hub, parameter){
 #' \dontrun{
 #' do_aquazis_query("https://aquazis.example.com/get_zr", list(f_parameter="Abfluss"))
 #' }
-do_aquazis_query<-function(hub,parameter){
+create_aquazis_query<-function(hub,parameter){
   query_string <- paste0(
     names(parameter), "=", vapply(parameter, URLencode, character(1), reserved = TRUE),
     collapse = "&"
   )
-  
+
   full_url <- paste0(hub, "?", query_string)
   return(curl(full_url))
 }
@@ -139,22 +168,22 @@ get_recent_eta_curves<-function(vec){
 
 
 #Available Parameters
-#[1] ""                     "C * Wurzel(I)"        "Batteriespannung"     "Q=C*Wurzel(I)*P"      "Potenzfunktion_12"    "Q = vm * A"          
-#[7] "Abfluss"              "Abflussdifferenz"     "Abflussfülle"         "Abflusskurven"        "Abflusskurve"         "Abflussmessergebnis" 
-#[13] "Abflussspende"        "Abfluss_Beurteilung"  "AEO"                  "Akimaspline_11"       "Akimaspline_8"        "Akkuspannung"        
-#[19] "Bedeckungsgrad"       "Begleitrechnung"      "Behälterinhalt"       "Bemess.-Niederschlag" "benetzter Umfang"     "Beta-Wert"           
-#[25] "Betriebsspannung"     "Breite"               "Dampfdruck"           "dbz"                  "Delta W"              "Durchfluss"          
-#[31] "Erdbodentemperatur"   "Etawert"              "Fließgeschwindigkeit" "Fließquerschnitt"     "Fließquerschnitt(St)" "Förderstrom"         
-#[37] "Fuelle"               "Gebietsniederschlag"  "Geschwindigkeit"      "Globalstrahlung"      "GOK"                  "GSM-Signalqualität"  
-#[43] "Himmelsstrahlung"     "HQFülle(1)"           "HQFülle(10)"          "HQFülle(100)"         "HQFülle(2)"           "HQFülle(20)"         
-#[49] "HQFülle(25)"          "HQFülle(5)"           "HQFülle(50)"          "Hydraulischer Radius" "Klappenstellung"      "Krautwuchs"          
-#[55] "kvonW"                "KvonW"                "Leistungsfähigkeit"   "Leitfähigkeit"        "Luftdruck"            "Luftfeuchte"         
-#[61] "Luftfeuchte_rel"      "Lufttemperatur"       "Manning-Strickler"    "Nasstemperatur"       "Neuschneehöhe"        "Niederschlag"        
-#[67] "Niederschlag N0"      "Niederschlag_Inhalt"  "Oberflächengeschw."   "Parameter_US_Anlage"  "Pegel"                "Pegelnullpunkt"      
-#[73] "Pegelwasserstan"      "Profilbeiwert"        "Querprofil"           "Schalldruckpegel_dB_" "Schneehöhe"           "Sickerwasser"        
-#[79] "Signalstärke"         "Sonnenscheindauer"    "Speicherinhalt"       "Status"               "Stauwert"             "Teilfüllungskurve"   
-#[85] "Teilfüllung_6"        "Teilfüllung_11"       "Teilfüllung_7"        "Teilfüllung_8"        "Temperatur"           "Temperatur_Luft"     
-#[91] "Verdunstung"          "Verdunstung_Haude"    "Wasseräquivalent"     "Wasserspiegelgefälle" "Wasserstand"          "WasserstandNHN"      
-#[97] "WasserstandNN"        "Wasserstandkurve"     "Wasserstand_O"        "Wasserstand_U"        "Wassertemperatur"     "Wassertiefe"         
-#[103] "Windgeschwindigkeit"  "Windrichtung"         "Windsektor"           "Windstärke"   
+#[1] ""                     "C * Wurzel(I)"        "Batteriespannung"     "Q=C*Wurzel(I)*P"      "Potenzfunktion_12"    "Q = vm * A"
+#[7] "Abfluss"              "Abflussdifferenz"     "Abflussfülle"         "Abflusskurven"        "Abflusskurve"         "Abflussmessergebnis"
+#[13] "Abflussspende"        "Abfluss_Beurteilung"  "AEO"                  "Akimaspline_11"       "Akimaspline_8"        "Akkuspannung"
+#[19] "Bedeckungsgrad"       "Begleitrechnung"      "Behälterinhalt"       "Bemess.-Niederschlag" "benetzter Umfang"     "Beta-Wert"
+#[25] "Betriebsspannung"     "Breite"               "Dampfdruck"           "dbz"                  "Delta W"              "Durchfluss"
+#[31] "Erdbodentemperatur"   "Etawert"              "Fließgeschwindigkeit" "Fließquerschnitt"     "Fließquerschnitt(St)" "Förderstrom"
+#[37] "Fuelle"               "Gebietsniederschlag"  "Geschwindigkeit"      "Globalstrahlung"      "GOK"                  "GSM-Signalqualität"
+#[43] "Himmelsstrahlung"     "HQFülle(1)"           "HQFülle(10)"          "HQFülle(100)"         "HQFülle(2)"           "HQFülle(20)"
+#[49] "HQFülle(25)"          "HQFülle(5)"           "HQFülle(50)"          "Hydraulischer Radius" "Klappenstellung"      "Krautwuchs"
+#[55] "kvonW"                "KvonW"                "Leistungsfähigkeit"   "Leitfähigkeit"        "Luftdruck"            "Luftfeuchte"
+#[61] "Luftfeuchte_rel"      "Lufttemperatur"       "Manning-Strickler"    "Nasstemperatur"       "Neuschneehöhe"        "Niederschlag"
+#[67] "Niederschlag N0"      "Niederschlag_Inhalt"  "Oberflächengeschw."   "Parameter_US_Anlage"  "Pegel"                "Pegelnullpunkt"
+#[73] "Pegelwasserstan"      "Profilbeiwert"        "Querprofil"           "Schalldruckpegel_dB_" "Schneehöhe"           "Sickerwasser"
+#[79] "Signalstärke"         "Sonnenscheindauer"    "Speicherinhalt"       "Status"               "Stauwert"             "Teilfüllungskurve"
+#[85] "Teilfüllung_6"        "Teilfüllung_11"       "Teilfüllung_7"        "Teilfüllung_8"        "Temperatur"           "Temperatur_Luft"
+#[91] "Verdunstung"          "Verdunstung_Haude"    "Wasseräquivalent"     "Wasserspiegelgefälle" "Wasserstand"          "WasserstandNHN"
+#[97] "WasserstandNN"        "Wasserstandkurve"     "Wasserstand_O"        "Wasserstand_U"        "Wassertemperatur"     "Wassertiefe"
+#[103] "Windgeschwindigkeit"  "Windrichtung"         "Windsektor"           "Windstärke"
 
