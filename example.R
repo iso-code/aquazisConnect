8c840df3a5a73059e57a0595d47a5490bc1a7a29
Marcel <marcel.gaj@lanuk.nrw.de>
2025-12-19 11:29:30 +0100

    added metadata parallel query

--- example.R
+++ example.R
@@ -9,7 +9,6 @@ library(devtools)
 library(furrr)
 options(scipen=999)
 
-
 install_github("iso-code/aquaZisConnect")
 library(aquazisConnect)
 hub<-""
@@ -19,15 +18,38 @@ setwd("aquazisConnect")
 check_hub_connection(hub)
 
 #aquire all station data from aquaZis
-as_meta<-get_aquazis_meta(hub, shared_data, logs)
-f_station<-as_meta %>% filter(NAME=="Ahmsen")
+zrlist<-get_aquazis_zrlist(hub,type="P", parameter="Wasserstand")
+orte<-as.numeric(zrlist$ort)
+orte<-orte[!is.na(orte)]
+
+plan(multisession, workers = parallel::detectCores() - 1)
+
+meta_query_parallel <- function(hub, ort) {
+#  Sys.sleep(1)
+  get_aquazis_meta(hub, ort)
+}
+
+if(!check_hub_connection(hub)) {
+  meta_list <- readr::read_rds(file.path(shared_data, "aquazis_stations.rds")) %>%
+    split(.$ORT)
+} else {
+  meta_list <- future_map_dfr(orte, ~meta_query_parallel(hub, .x))
+  fallback_meta(meta_list, shared_data)
+}
+
+meta_tibble <- dplyr::bind_rows(meta_list)
+
+################################################
+f_station<-as_meta %>% filter(NAME=="Rheda")
 ort<-as.character(f_station$ORT)
+ort
 ############################################
 #get specific zrList for all water levels
 
-zrlist<-get_aquazis_zrlist(hub, parameter="Wasserstand", type="P")
-zrlist
+zrlist<-get_aquazis_zrlist(hub,ort,type="P", parameter="Wasserstand")
+head(zrlist)
 
+zrlist<-get_aquazis_zrlist(hub,ort, parameter="Abfluss")
 #get timeseries values from zrid in period
 ###############################################
 begin <- Sys.time()-(60*60*24)*365
@@ -38,57 +60,31 @@ intervall<-"l"
 #zrid<-"48415"
 #if timeseries produces only NA no data is available for the period.
 #Find the end of validated data here:
-verified_to<-get_az_valid_to(hub, zrid, begin, end, intervall = "l", stepsize = 365, max_retries = 10)
+verified_to<-get_az_valid_to(hub, zrids[1], begin, end, intervall = "l", stepsize = 365, max_retries = 10)
 verified_to
 
 verified_to<-get_verified_periods(hub, zrids, begin, end, intervall = "l", stepsize = 365, max_retries = 10) 
 verified_to
 
 final_table <- get_verified_periods_parallel(hub, zrids, begin, end, intervall = "l", stepsize = 365, max_retries = 10)
-
+final_table
 
 ######################################################
-begin <- Sys.time()-(60*60*24)*1665
-end <- Sys.time()
-zrid<-"48266"
-zr_data<-get_aquazis_zr(hub, zrid, begin, end)
+begin <- final_table$start[1]
+end <- final_table$valid[1]
+zr_data<-get_aquazis_zr(hub, zrids[1], begin, end)
 zr<-extract_az_ts(zr_data,"l")
 
-########################################################
-# Rating Curves
-########################################################
-
-begin <- Sys.time()-(60*60*24)*3650
-end <- Sys.time()
-
-as_meta$NAME
-ORT<-as_meta %>% filter(NAME=="Ahmsen") %>% distinct(ORT) %>% as.character()
-
-zrlist<-get_aquazis_zrlist(hub,ORT,parameter="Wasserstand",type="mes")
-zrid<-zrlist$zrid
-zr <-get_aquazis_zr(hub, zrid, begin = begin, end = end)
-w<-zr$data$Daten
-
-zrlist<-get_aquazis_zrlist(hub,ORT,parameter="Abfluss",type="mes")
-zrid<-zrlist$zrid
-zr <-get_aquazis_zr(hub, zrid, begin = begin, end = end)
-q<-zr$data$Daten
-
-wq<-merge(w,q,by="V1") %>% tibble()
-wq$V2.x<-as.numeric(wq$V2.x)
-wq$V2.y<-as.numeric(wq$V2.y)
-plot(wq$V2.x,wq$V2.y)
 
 
-###Alle Messungnen
-
+###########################################
 ort<-as_meta %>% filter(NAME=="Ahmsen")
-
+ort
 zrlist<-get_aquazis_zrlist(hub,ort$ORT,type="mes")
 pars<-zrlist$parameter
-
+zrlist
 results_list <- lapply(zrlist$zrid, function(zrid) {
-  Sys.sleep(1)  # Pause wegen API
+  #Sys.sleep(1)  # Pause wegen API
 extract_az_ts(get_aquazis_zr(hub, zrid, begin = begin, end = end))
 })
 names(results_list) <-  zrlist$einheit
